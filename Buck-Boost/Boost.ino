091a46f83ce266cf6fca2cba33da8924ac40e02f
William <utbwilost1@skola.malmo.se>
2022-11-07 14:18:55 +0100

    commence

--- Buck-Boost/Boost.ino
+++ Buck-Boost/Boost.ino
@@ -1,11 +1,18 @@
+#include "config.h"
+
 int LED_STATE = 0;
 
+AdafruitIO_Feed *batPercent = io.feed("batPercent");
+
 void setup(){
+    Serial.begin(115200);
     pinMode(13, OUTPUT);
 }
 
 void loop(){
+    io.run();
     LED_STATE = abs(LED_STATE - 1);
     digitalWrite(13, LED_STATE);
     delay(2000);
+    
 }
